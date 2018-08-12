a7e4cfda309b3dbc5562035e43b9114771f6fc60
vikas kumar <vikas.shetty.456@gmail.com>
2018-08-12 20:45:16 +0530

    middleware created

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEdit;
 use App\Role;
 use Illuminate\Http\Request;
 use App\Http\Requests;
@@ -39,7 +40,13 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         //
-        $input = $request->all();
+        if(trim($request->password)==''){
+             $input = $request->except('password');
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+
+        }
         if($file = $request->file('photo_id')){
                    $name = time().$file->getClientOriginalName();
 
@@ -49,8 +56,6 @@ class AdminUsersController extends Controller
                    $input['photo_id'] = $photo->id;
 
         }
-
-        $input['password'] = bcrypt($request->password);
 
         User::create($input);
         return redirect('/admin/users');
@@ -83,7 +88,9 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name','id')->all();
+        return view('admin.users.edit', compact('user','roles'));
     }
     /**
      * Update the specified resource in storage.
@@ -92,9 +99,32 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEdit $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+        if(trim($request->password)==''){
+            $input = $request->except('password');
+        }else{
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+        if($file = $request->file('photo_id')){
+            $name = time().$file->getClientOriginalName();
+
+            $file->move('images',$name);
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+
+        $user->update($input);
+        return redirect('admin/users');
+
+
     }
     /**
      * Remove the specified resource from storage.
--- app/Photo.php
+++ app/Photo.php
@@ -8,4 +8,10 @@ class Photo extends Model
 {
     //
      protected $fillable = ['file'];
+
+     protected $uploads = '/images/';
+
+     public function getFileAttribute($photo){
+                return $this->uploads.$photo;
+     }
 }
